231763ad2ee8ef6e623806f6be1ddb15e65dfe30
Tyler Trinh <tylervtrinh@gmail.com>
2019-10-08 18:36:29 -0700

    Add schema for database.

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tokis(
+    id SERIAL PRIMARY KEY,
+    sprite TEXT,
+    name TEXT,
+    img_link TEXT,
+    height REAL,
+    weight REAL,
+    fly REAL,
+    fight REAL,
+    fire REAL,
+    water REAL,
+    electic REAL,
+    ice REAL,
+    total REAL
+    trainer_name TEXT
+);
